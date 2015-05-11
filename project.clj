ab33a1600e6b78ff6a52e73fedf0bb43cf65356d
Moritz Ulrich <moritz.ulrich@bevuta.com>
2015-05-11 20:00:01 +0200

    Fix project url in project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.bevuta/lpd "0.1.0-SNAPSHOT"
   :description "Library implementing the server-side of RFC 1179 (Line Printer Daemon)"
-  :url "https://github.com/bevuta/lpd"
+  :url "https://github.com/bevuta/clojure-lpd"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
